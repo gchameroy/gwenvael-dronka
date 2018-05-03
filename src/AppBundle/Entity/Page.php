e8b8e94b36266a6fbc88ba2cc45387b70a8ba5da
Jennifer Calipel <jennifer.calipel@gmail.com>
2018-05-03 22:19:21 +0200

    Gestion des pages #GDAC-7
    Gestion des pages #GDAC-7

---
+++ src/AppBundle/Entity/Page.php
@@ -0,0 +1,263 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Page
+ *
+ * @ORM\Table(name="page")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PageRepository")
+ */
+class Page
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(length=128, unique=true)
+     */
+    private $slug;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=1000)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title_seo", type="string", length=255, nullable=true)
+     */
+    private $titleSeo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description_seo", type="string", length=1000, nullable=true)
+     */
+    private $descriptionSeo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="published_at", type="datetime", nullable=true)
+     */
+    private $publishedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Page
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Page
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Page
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set titleSeo
+     *
+     * @param string $titleSeo
+     *
+     * @return Page
+     */
+    public function setTitleSeo($titleSeo)
+    {
+        $this->titleSeo = $titleSeo;
+
+        return $this;
+    }
+
+    /**
+     * Get titleSeo
+     *
+     * @return string
+     */
+    public function getTitleSeo()
+    {
+        return $this->titleSeo;
+    }
+
+    /**
+     * Set descriptionSeo
+     *
+     * @param string $descriptionSeo
+     *
+     * @return Page
+     */
+    public function setDescriptionSeo($descriptionSeo)
+    {
+        $this->descriptionSeo = $descriptionSeo;
+
+        return $this;
+    }
+
+    /**
+     * Get descriptionSeo
+     *
+     * @return string
+     */
+    public function getDescriptionSeo()
+    {
+        return $this->descriptionSeo;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Page
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set publishedAt
+     *
+     * @param \DateTime $publishedAt
+     *
+     * @return Page
+     */
+    public function setPublishedAt($publishedAt)
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get publishedAt
+     *
+     * @return \DateTime
+     */
+    public function getPublishedAt()
+    {
+        return $this->publishedAt;
+    }
+
+    /**
+     * Get isPublished
+     *
+     * @return boolean
+     */
+    public function isPublished()
+    {
+        return $this->publishedAt ? true : false;
+    }
+}
