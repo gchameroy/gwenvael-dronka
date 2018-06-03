51acff141fa60b07b3ddd460689cf7292a192d3d
Geoffrey Chameroy <geoffrey.chameroy@gmail.com>
2018-06-03 17:09:29 +0200

    GDAC-28 (Add dynamic menu) (#38)

--- tests/functional/Website/WebsiteStaticPageCest.php
+++ tests/functional/Website/WebsiteStaticPageCest.php
@@ -8,18 +8,19 @@ class WebsiteStaticPageCest
     public function tryHome(FunctionalTester $I)
     {
         $I->amOnPage('/');
-        $I->seeCurrentUrlEquals('/');
         $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeCurrentUrlEquals('/');
         $I->see('Une méthode douce', 'h1');
         $I->see('D\'éducation canine', 'h2');
         $I->seeNumberOfElements('.social li', FixtureHelper::NB_SETTING_SOCIAL_NETWORK);
+        $I->seeNumberOfElements('.nav-menu li', FixtureHelper::NB_MENU);
     }
 
     public function tryPrices(FunctionalTester $I)
     {
         $I->amOnPage('/tarifs');
-        $I->seeCurrentUrlEquals('/tarifs');
         $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeCurrentUrlEquals('/tarifs');
         $I->see('Tarifs', 'h2');
         $I->see('Tarifs', 'h3');
         $I->seeNumberOfElements('#box-prices .Pricing-box', FixtureHelper::NB_PRICE);
@@ -30,16 +31,16 @@ class WebsiteStaticPageCest
     public function trySites(FunctionalTester $I)
     {
         $I->amOnPage('/sites');
-        $I->seeCurrentUrlEquals('/sites');
         $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeCurrentUrlEquals('/sites');
         $I->see('Sites', 'h2');
     }
 
     public function tryLessons(FunctionalTester $I)
     {
         $I->amOnPage('/cours');
-        $I->seeCurrentUrlEquals('/cours');
         $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeCurrentUrlEquals('/cours');
         $I->see('Nos Cours', 'h2');
     }
 }
