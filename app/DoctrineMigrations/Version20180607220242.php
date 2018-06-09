c17634b7f738b1461f2bad039919eebf992f4dc2
geoffrey-chameroy <geoffrey.chameroy@gmail.com>
2018-06-09 21:37:56 +0200

    Hotfix database

--- app/DoctrineMigrations/Version20180607220242.php
+++ app/DoctrineMigrations/Version20180607220242.php
@@ -10,12 +10,10 @@ final class Version20180607220242 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         $this->addSql('ALTER TABLE page ADD image VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE social_network RENAME INDEX uniq_efff52212b36786b TO UNIQ_EFFF5221EA750E8');
     }
 
     public function down(Schema $schema) : void
     {
         $this->addSql('ALTER TABLE page DROP image');
-        $this->addSql('ALTER TABLE social_network RENAME INDEX uniq_efff5221ea750e8 TO UNIQ_EFFF52212B36786B');
     }
 }
